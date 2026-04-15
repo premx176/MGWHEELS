0cdef61c1461f67d5427e6b221c8285790f38cce
premx176 <premsalve547@gmail.com>
2026-04-15 23:01:12 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{padding: "40px", fontFamily: "Arial"}}>
+      <h1>MG WHEELS</h1>
+      <p>Your website is now live 🚀</p>
+    </div>
+  );
+}
